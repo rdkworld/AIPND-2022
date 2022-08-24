1d26d8d79a0c51885c48d2a78b2813f942f3d748
rdkworld <46112652+rdkworld@users.noreply.github.com>
2022-08-24 09:39:40 -0700

    Added writer changes

--- Generalized/engine.py
+++ Generalized/engine.py
@@ -191,5 +191,28 @@ def train(model: torch.nn.Module,
         results["test_loss"].append(test_loss)
         results["test_acc"].append(test_acc)
 
+        ### New: Experiment tracking ###
+        # Add loss results to SummaryWriter
+        writer.add_scalars(main_tag="Loss", 
+                           tag_scalar_dict={"train_loss": train_loss,
+                                            "test_loss": test_loss},
+                           global_step=epoch)
+
+        # Add accuracy results to SummaryWriter
+        writer.add_scalars(main_tag="Accuracy", 
+                           tag_scalar_dict={"train_acc": train_acc,
+                                            "test_acc": test_acc}, 
+                           global_step=epoch)
+        
+        # Track the PyTorch model architecture
+        writer.add_graph(model=model, 
+                         # Pass in an example input
+                         input_to_model=torch.randn(32, 3, 224, 224).to(device))
+
+    # Close the writer
+    writer.close()
+    
+    ### End new ###
+
     # Return the filled results at the end of the epochs
     return results
